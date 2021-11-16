ff0edc92617063fc9d78a616d7f03d0d3549e3b6
Ranvirsing <81947226+Ranvirsing@users.noreply.github.com>
2021-11-16 18:59:00 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,render_template,request
+from flask import Flask,jsonify,render_template,request,url_for
 app=Flask(__name__,static_folder='/static/')
 @app.route('/')
 def apps():
