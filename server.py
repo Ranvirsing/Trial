c87f2616d21d12857f9abe7a4532643696cd1550
Ranvirsing <81947226+Ranvirsing@users.noreply.github.com>
2021-11-20 16:37:20 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,19 @@ app=Flask(__name__)
 @app.route('/')
 def apps():
     return render_template('app.html')
-
+@app.route('/get_est_price', methods = ['POST'])
+def get_est_price():
+    year=int(request.form['year'])
+    km=int(request.form['km'])
+    tran=request.form['tran']
+    mileage=int(request.form['mileage'])
+    fuel=request.form['fuel'] 
+    response = jsonify({
+        'est_price':util.get_est_price(year,km,tran,mileage,fuel)
+        })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    
+    return response,render_template('app.html')
 if __name__ == '__main__':
     apps()
     app.run()
